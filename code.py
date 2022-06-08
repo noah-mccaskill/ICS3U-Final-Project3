419cbdbd9c02ed1c49d9d2d022925df71f663133
noah-mccaskill <94465036+noah-mccaskill@users.noreply.github.com>
2022-06-08 10:30:40 -0400

    Create code.py

---
+++ code.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+# Created by: Noah McCaskill
+# Created on: June 2022
+# This will display the "Space Aliens" program on EdgeBadge
+
+import ugame
+import stage
+
+
+def game_scene():
+    # this function is for the main game game_scene
+
+    # image banks for CircuitPython
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
+
+    # set background to image 0 in the image Bank
+    # and the side (10x8 tiles of size 16x16)
+    background = stage.Grid(image_bank_background, 10, 8)
+    
+    # a sprite that will be updated every frame
+    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
+
+    # create a stage for the background to show up on
+    # and set the frame rate to 60fps
+    game = stage.Stage(ugame.display, 60)
+    # set the layers of all sprites, items show up in order
+    game.layers = [ship] + [background]
+    # render all sprites 
+    # most likely I will render the background once per game scene 
+    game.render_block()
+
+    # repeat forever, game loop
+    while True:
+        # get user input
+        
+        # update game logic
+        
+        # redraw sprite
+        game.render_sprites([ship])
+        game.tick # wait until refresh rate finishes
+
+if __name__ == "__main__":
+    game_scene()
+    
